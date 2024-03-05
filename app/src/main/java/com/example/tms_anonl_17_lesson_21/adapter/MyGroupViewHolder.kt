50f4a91728cc18a44813a93b7c10b2cb45aaaf6c
maratbelitski <marat.belitckiy2010@yandex.com>
2024-03-05 21:16:33 +0300

    fix

--- app/src/main/java/com/example/tms_anonl_17_lesson_21/adapter/MyGroupViewHolder.kt
+++ app/src/main/java/com/example/tms_anonl_17_lesson_21/adapter/MyGroupViewHolder.kt
@@ -1,6 +1,7 @@
 package com.example.tms_anonl_17_lesson_21.adapter
 
 import android.content.Context
+import android.content.res.ColorStateList
 import android.content.res.Resources
 import android.content.res.Resources.Theme
 import android.view.LayoutInflater
@@ -25,6 +26,11 @@ class MyGroupViewHolder(private val binding: NoteItemGroupBinding) :
 
     fun bind(group: Group, click: ((Group) -> Unit)?) {
         binding.inputName.text = group.name
+
+        //смена цветов представления через контекст
+        binding.cardNote.backgroundTintList =
+            ColorStateList.valueOf(ContextCompat.getColor(itemView.context,R.color.card_back_group_blue))
+        binding.inputName.setTextColor(ContextCompat.getColor(itemView.context,R.color.white))
 
         itemView.setOnClickListener {
             click?.invoke(group)
